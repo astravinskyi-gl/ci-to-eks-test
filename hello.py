359f8864661b276464c17f1e7f511b02fcf97450
astravinskyi-gl <94978105+astravinskyi-gl@users.noreply.github.com>
2022-03-19 20:29:32 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return 'Hello, it is testing mthf'
+@app.route('/')
+def index():
+    return 'Hello, it is testing mthf'
 
-# app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=81)
 
-def print_hi(name):
-    i = 1
-    while i == 1:
-        print("Hello " + name + " , it is the message from the test app")
+# def print_hi(name):
+#     i = 1
+#     while i == 1:
+#         print("Hello " + name + " , it is the message from the test app")
     
-print_hi("everyone")
+# print_hi("everyone")
