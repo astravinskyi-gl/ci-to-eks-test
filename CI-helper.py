872d7ebde5604db90a4c9f64b157a567474ff7ec
Vasyl Akimov <vasyl.akimov@globallogic.com>
2022-04-11 11:19:03 +0000

    minor: minor test

--- CI-helper.py
+++ CI-helper.py
@@ -8,10 +8,8 @@ def index():
 
 app.run(host='0.0.0.0', port=60080)
 
-##
 # def print_hi(name):
 #     i = 1
 #     while i == 1:
 #         print("Hello " + name + " , it is the message from the test app")
-    
 # print_hi("everyone")
